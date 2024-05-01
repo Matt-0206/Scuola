03c26d17b0963742cbaf810e112dcc4dc67ca982
Matteo_Alliu <malliu431@gmail.com>
2024-05-01 17:04:27 +0200

    01/05/2024

--- Processing/Array_And_Objects/AO_07/AO_07.js
+++ Processing/Array_And_Objects/AO_07/AO_07.js
@@ -20,8 +20,23 @@ function draw() {
     
     rect( rectObjs[i].xpos, rectObjs[i].ypos, 50, 24 );
       
-    rectObjs[i].ypos += rectObjs[i].speed;
+    rectObjs[i].ypos+= rectObjs[i].up_down; 
     
+    rectObjs[i].xpos += rectObjs[i].left_right;
+
+    // se "ypos" e "xpos" sono maggiori dell'altezza o della larghezza oppure se "ypos" e "xpos" sono minori di 0 (confini del canvas)
+    // moltiplica "up_down" o "left_right" per -1 (oppposto della direzione originale)
+
+    if( rectObjs[i].ypos > height || rectObjs[i].ypos < 0 ){ 
+      
+      rectObjs[i].up_down *= -1;  
+      
+    }else if( rectObjs[i].xpos > width || rectObjs[i].xpos < 0 ){
+      
+      rectObjs[i].left_right *= -1;
+      
+    }
+
   }
   
 }
@@ -29,8 +44,10 @@ function draw() {
 
 function mousePressed(){
 
-  rectObjs.push({ xpos: mouseX, ypos: mouseY, fillColor: random(255), speed: random(10) });
+//  rectObjs.push({ xpos: mouseX, ypos: mouseY, fillColor: random(255), speed: random(10)});
   
+    rectObjs.push({ xpos: mouseX, ypos: mouseY, fillColor: random(255), up_down: random(-8, 8), left_right: random(-8, 8)});
+
   console.log(rectObjs);
   
 }
