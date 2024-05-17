eae1f46512321729b452679502611ef2589453ed
Matteo_Alliu <malliu431@gmail.com>
2024-05-17 09:59:14 +0200

    17/05/2024

---
+++ HTML/People in space/script.js
@@ -0,0 +1,45 @@
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const STAR_COUNT = 400
+let result = ""
+for(let i = 0; i < STAR_COUNT; i++){
+    result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 1)}px ${randomNumber(0, 1)}px #fff,`
+}
+
+
+//------------------------------------------------------------
+
+async function populate(){
+
+    const requestURL = "https://matt-0206.github.io/my-repository/astros.json"; // gli viene assegnato un link 
+
+    const request = new Request(requestURL) // crea un nuovo oggetto
+
+    const response = await fetch(request); // 
+    
+    const spacePeople = await response.json(); //
+
+    populateTable(spacePeople);
+
+}
+
+
+function populateTable(obj){
+
+    //const para1 = document.getElementById('1');
+    //para1.textContent = obj.name;
+
+    const people = obj.people;
+
+    for(const item of people){
+
+        const para = document.getElementById('1');
+        para.textContent = item.name;
+        
+    }
+
+}
+
+populate();
